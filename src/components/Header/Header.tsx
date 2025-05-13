5640f1ef04570c96186bf56cb332ecb9f17a2c7a
hungirlor123 <hungdz369@gmail.com>
2025-05-14 01:59:41 +0700

    fix typescript

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import { Button, Typography } from "antd";
 
-export default function Header() {
+const Header = () =>  {
   return (
     <header className="bg-white shadow-md p-4 flex justify-between items-center">
       <Typography.Title level={3} style={{ margin: 0, color: "#1677ff" }}>
@@ -16,3 +16,5 @@ export default function Header() {
     </header>
   );
 }
+
+export default Header;
